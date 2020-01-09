77822db02851299a1d07b6e4a7d4b21aec0858f2
yjg1013 <whdrms6098@ojworld.co.kr>
2020-01-09 12:50:43 +0900

    logim

---
+++ backend/src/api/books/index.js
@@ -0,0 +1,13 @@
+const Router = require('koa-router');
+
+const books = new Router();
+const booksCtrl = require('./books.controller');
+
+books.get('/', booksCtrl.list);
+books.get('/:id', booksCtrl.get);
+books.post('/', booksCtrl.create);
+books.delete('/', booksCtrl.delete);
+books.put('/', booksCtrl.replace);
+books.patch('/', booksCtrl.update);
+
+module.exports = books;
---
+++ frontend/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import Root from './Root';
+import configureStore from 'redux/configureStore';
+import { AppContainer } from 'react-hot-loader';
+
+const store = configureStore();
+
+const render = Component => {
+    ReactDOM.render(
+        <AppContainer>
+            <Component store={store}/>
+        </AppContainer>,
+        document.getElementById('root')
+    );
+};
+
+render(Root);
+
+if(module.hot) {
+    module.hot.accept('./Root', () => render(Root));
+}
+serviceWorker.unregister();
